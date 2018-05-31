2b669957e6b44ad09f088d48a08b95a8befac3e4
Mozart <rafaelnunez@Rafaels-MacBook-Pro.local>
2018-05-31 02:07:31 -0400

    hw

---
+++ Pybank/Main.py
@@ -0,0 +1,58 @@
+# import libraries ( os , csv and sys)
+import os
+import csv
+import sys
+# '..','Pybank', 'budget_data_1.csv'
+# setting path
+csvpath =  os.path.join( '..','Pybank','budget_data_1.csv')
+
+# declaring variables to assing values 
+total_months = 0
+total_revenue = 0
+greatest_inc = -sys.maxsize - 1
+greatest_dec = sys.maxsize
+greatest_dec_date = ""
+greatest_inc_date = ""
+
+
+# reading the file in and opening the csv_file as a reader 
+with open(csvpath, newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    next(csvreader, None)
+
+    # counting months; finding total revenue; find greatest inc & dec in revenue
+    for row in csvreader:
+        total_months += 1
+        total_revenue += int(row[1])
+        if int(row[1]) >= greatest_inc:
+            greatest_inc = int(row[1])
+            greatest_inc_date = row[0]
+        if int(row[1]) <= greatest_dec:
+            greatest_dec = int(row[1])
+            greatest_dec_date = row[0]
+
+# calculating total average revenue change
+average_revenue_change = round(total_revenue/total_months, 2)
+
+# printing results to terminal ( fianancial Analysis, total months, revenue avr revenue , greatest inc/dec)
+print("Financial Analysis")
+print("--------------------------------------------")
+print("Total Months: " + str(total_months))
+print("Total Revenue: $" + str(total_revenue))
+print("Average Revenue Change: $" + str(average_revenue_change))
+print("Greatest Increase in Revenue: " + greatest_inc_date 
++ " ($" + str(greatest_inc) + ")")
+print("Greatest Decrease in Revenue: " + greatest_dec_date + 
+" ($" + str(greatest_dec) + ")")
+
+# creating the new txt file
+new_file = open("output.txt", "w")
+
+# writing the text file
+new_file.write("Financial Analysis \n")
+new_file.write("-------------------------------------------- \n")
+new_file.write("Total Months: " + str(total_months) + "\n")
+new_file.write("Total Revenue: $" + str(total_revenue) + "\n")
+new_file.write("Average Revenue Change: $" + str(average_revenue_change) + "\n")
+new_file.write("Greatest Increase in Revenue: " + greatest_inc_date + " ($" + str(greatest_inc) + ")" + "\n")
+new_file.write("Greatest Decrease in Revenue: " + greatest_dec_date + " ($" + str(greatest_dec) + ")" + "\n")
---
+++ Pypoll/Main.py
@@ -0,0 +1,67 @@
+# import libraries os and csv 
+import os
+import csv
+
+# set file path to conect data resource 
+csvpath = os.path.join('..','Pypoll', 'election_data_1.csv')
+
+# declaring variables vote count, candidates, candidates %, winner and winner count 
+vote_count = 0
+candidates = {}
+candidates_percent = {}
+winner = ""
+winner_count = 0
+
+# reading file as csv.reader 
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    next(csvreader, None)
+
+    # finding total vote count; finding individual vote counts
+    for row in csvreader:
+        vote_count += 1
+        if row[2] in candidates.keys():
+            candidates[row[2]] += 1
+        else:
+            candidates[row[2]] = 1
+
+# percentages for each candidate
+for key, value in candidates.items():
+    candidates_percent[key] = round((value/vote_count) * 100, 2)
+
+# finding the winner
+for key in candidates.keys():
+    if candidates[key] > winner_count:
+        winner = key
+        winner_count = candidates[key]
+
+# tests
+# print(total_vote_count)
+# print(candidates)
+# print(candidates_percent)
+# print(winner)
+
+# printing to the terminal
+print("Election Results")
+print("-------------------------------------")
+print("Total Votes: " + str(vote_count))
+print("-------------------------------------")
+for key, value in candidates.items():
+    print(key + ": " + str(candidates_percent[key]) + "% (" + str(value) + ")")
+print("-------------------------------------")
+print("Winner: " + winner)
+print("-------------------------------------")
+
+# creating new text file
+new_file = open("Output.txt", "w")
+
+# writing the new file
+new_file.write("Election Results \n")
+new_file.write("------------------------------------- \n")
+new_file.write("Total Votes: " + str(vote_count) + "\n")
+new_file.write("------------------------------------- \n")
+for key, value in candidates.items():
+    new_file.write(key + ": " + str(candidates_percent[key]) + "% (" + str(value) + ") \n")
+new_file.write("------------------------------------- \n")
+new_file.write("Winner: " + winner + "\n")
+new_file.write("------------------------------------- \n")
